16c3eec7705b72b877802db94dcb6561118f8c74
Alberto Mijares <alberto.mijares@edorasware.com>
2016-02-29 22:45:09 +0100

    (chore): add initial package.json and webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+require('babel-polyfill');
+
+var path = require('path'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: src('index.js'),
+    output: {
+        path: 'dist',
+        filename: 'app.js'
+    },
+    resolve: {
+        alias: {
+            widgets: src('components/widgets'),
+            services: src('services')
+        }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'stage-0'],
+                    plugins: [
+                        'transform-decorators-legacy'
+                    ]
+                }
+            },
+            {test: /\.css$/, loader: ExtractTextPlugin.extract('css')},
+            {test: /\.(woff|woff2|ttf|eot|svg)$/, loader: 'file?name=font/[name].[ext]'}
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('app.css'),
+        new HtmlWebpackPlugin({
+            template: src('index.html'),
+            inject: 'body'
+        })
+    ]
+};
+
+function src(fileName) {
+    return path.join(__dirname, 'src', fileName);
+}
